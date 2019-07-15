2d8d4dea001f330a2d79520a41db400ccd3be705
Danny Lee <dreamer@panix.com>
2019-07-15 00:50:41 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,7 +1,7 @@
 # Code your solution here!
 
 def run_guessing_game
-  answer = rand(1..6)
+  answer = rand(6) + 1
   puts "Please guess a number"
   response = gets.chomp
   if response.to_i == answer
